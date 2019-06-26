f8f6171b1f0466c6a23c7fbb10eba12cf625c365
Matthew Yates <Mat.C.Yates@gmail.com>
2019-06-26 16:28:31 -0400

    finished basic view implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-import { User } from './models/User';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
+import { User, UserProps } from './models/User';
+
+const root = document.getElementById('root');
+if (root) {
+	const users = new Collection(
+		'http://localhost:3000/users',
+		(json: UserProps) => {
+			return User.buildUser(json);
+		}
+	);
+	users.on('change', () => {
+		return new UserList(root, users);
+	});
+	users.fetch();
+}
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,17 +1,39 @@
-export class UserForm {
-	constructor(public parent: Element) {}
+import { User, UserProps } from '../models/User';
+import { View } from './View';
+
+export class UserForm extends View<User, UserProps> {
+	eventsMap(): { [key: string]: () => void } {
+		return {
+			'click:.set-age': this.onSetAgeClick,
+			'click:.set-name': this.onSetNameClick,
+			'click:.save-model': this.onSaveClick
+		};
+	}
+
+	onSaveClick = (): void => {
+		this.model.save();
+	};
+
+	onSetAgeClick = (): void => {
+		this.model.setRandomAge();
+	};
+
+	onSetNameClick = () => {
+		const input = this.parent.querySelector('input');
+		if (input) {
+			const name = input.value;
+
+			this.model.set({ name });
+		}
+	};
 
 	template(): string {
 		return `
     <div>
-      <h1>User Form</h1>
-      <input />
+      <input placeholder="${this.model.get('name')}" />
+      <button class="set-name">Change name</button>
+      <button class="set-age">Set random age</button>
+      <button class=save-model>Save</button>
     </div>`;
-	}
-	render(): void {
-		const templateElement = document.createElement('template');
-		templateElement.innerHTML = this.template();
-
-		this.parent.append(templateElement.content);
 	}
 }
